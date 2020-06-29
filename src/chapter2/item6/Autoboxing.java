d1c5f8f09a90f46b284bf4fb2ea47a817b434c1d
Pritpal Singh Banga <Pritpal25@users.noreply.github.com>
2020-06-29 08:58:21 -0700

    Item 6 : Reusing objects and avoiding autoboxing for performance enhancements.

---
+++ src/chapter2/item6/Autoboxing.java
@@ -0,0 +1,57 @@
+package chapter2.item6;
+
+public class Autoboxing {
+    private static Long slowSum(int reps) {
+        Long sum = 0L;
+
+        for(int i = 0; i < reps; i++) {
+            for (long l = 0; l < Integer.MAX_VALUE; l++) {
+                sum += l;
+            }
+        }
+
+        return sum;
+    }
+
+    private static long fastSum(int reps) {
+        long sum = 0L;
+
+        for(int i = 0; i < reps; i++) {
+            for (long l = 0; l < Integer.MAX_VALUE; l++) {
+                sum += l;
+            }
+        }
+
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int reps = Integer.parseInt(args[0]);
+
+        long start = System.nanoTime();
+        long sum = fastSum(reps);
+        long end = System.nanoTime();
+
+        System.out.println((end-start)/(1_000_000. * reps) + " micro secs");
+
+        if(sum == 42) {
+            System.out.println();
+        }
+
+        start = System.nanoTime();
+        sum = slowSum(reps);
+        end = System.nanoTime();
+
+        System.out.println((end-start)/(1_000_000. * reps) + " micro secs");
+
+        if(sum == 42) {
+            System.out.println();
+        }
+    }
+}
+
+/*
+Running this got a 10 times performance enhancement from primitive types.
+589.5782035 micro secs
+5353.3895035 micro secs
+ */
---
+++ src/chapter2/item6/RomanNumerals.java
@@ -0,0 +1,38 @@
+package chapter2.item6;
+
+import java.util.regex.Pattern;
+
+public class RomanNumerals {
+    static boolean isRomanNumeralSlow(String s) {
+        return s.matches("^(?=.)M*(C[MD]|D?C{0,3})"
+                + "(X[CL]|L?X{0,3})(I[XV]|V?I{0,3})$");
+    }
+
+    // Precompiled cached(static) variable for reuse.
+    private static final Pattern ROMAN = Pattern.compile(
+            "^(?=.)M*(C[MD]|D?C{0,3})"
+                    + "(X[CL]|L?X{0,3})(I[XV]|V?I{0,3})$");
+
+    static boolean isRomanNumeralFast(String s) {
+        return ROMAN.matcher(s).matches();
+    }
+
+    public static void main(String[] args) {
+        int numSets = Integer.parseInt(args[0]);
+        int numReps = Integer.parseInt(args[1]);
+        boolean b = false;
+        for (int i = 0; i < numSets; i++) {
+            long start = System.nanoTime();
+            for (int j = 0; j < numReps; j++) {
+                b ^= isRomanNumeralFast("MCMLXXVI");
+            }
+            long end = System.nanoTime();
+            System.out.println(((end - start) / (1_000. * numReps)) + " μs.");
+        }
+
+        // Since this is the only real usage of b, the compiler will make sure to keep updating b in statement 28
+        // Otherwise, an over eager compiler could just not update the value, since its never getting used elsewhere.
+        if (!b)
+            System.out.println();
+    }
+}
